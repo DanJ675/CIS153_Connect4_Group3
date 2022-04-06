afcc569474b8fd9b67f530c6a239a3e8d81003c9
DanJ675 <82305541+DanJ675@users.noreply.github.com>
2022-04-06 07:58:30 -0400

    Update Board.cs

--- CIS153_Connect4_Group3/CIS153_Connect4_Group3/Board.cs
+++ CIS153_Connect4_Group3/CIS153_Connect4_Group3/Board.cs
@@ -28,44 +28,44 @@ namespace CIS153_Connect4_Group3
             }
 
         }
-        public Square[,] showNextMoveForColumn(int playerNum, int column)
-        {
-            Square[,] outBoard = new Square[7, 6];
-            bool addedPiece = false;
-            //fill from bottom up
-            for(int c = 0; c < 7; c++)
-            {
-                for (int row = 0; row < 6; row++)
-                {
-                    if(c == column)
-                    {
-                        if (board[c, row].getPlayerNum() == 0 && !addedPiece)
-                        {
-                            Square square = new Square();
-                            addedPiece = true;
-                            square.setPlayerNum(playerNum);
-                            outBoard[c, row] = square;
-                        }
-                        else
-                        {
-                            Square square = new Square();
-                            square.setPlayerNum(board[c, row].getPlayerNum());
-                            outBoard[c, row] = square;
-                        }
+        //public Square[,] showNextMoveForColumn(int playerNum, int column)
+        //{
+        //    Square[,] outBoard = new Square[7, 6];
+        //    bool addedPiece = false;
+        //    //fill from bottom up
+        //    for(int c = 0; c < 7; c++)
+        //    {
+        //        for (int row = 0; row < 6; row++)
+        //        {
+        //            if(c == column)
+        //            {
+        //                if (board[c, row].getPlayerNum() == 0 && !addedPiece)
+        //                {
+        //                    Square square = new Square();
+        //                    addedPiece = true;
+        //                    square.setPlayerNum(playerNum);
+        //                    outBoard[c, row] = square;
+        //                }
+        //                else
+        //                {
+        //                    Square square = new Square();
+        //                    square.setPlayerNum(board[c, row].getPlayerNum());
+        //                    outBoard[c, row] = square;
+        //                }
 
-                    }
-                    else
-                    {
-                        Square square = new Square();
-                        square.setPlayerNum(board[c, row].getPlayerNum());
-                        outBoard[c, row] = square;
-                    }
-                }
+        //            }
+        //            else
+        //            {
+        //                Square square = new Square();
+        //                square.setPlayerNum(board[c, row].getPlayerNum());
+        //                outBoard[c, row] = square;
+        //            }
+        //        }
 
-            }
+        //    }
 
-            return outBoard;
-        }
+        //    return outBoard;
+        //}
         public void commitNextMoveForColumn(int playerNum, int column)
         {
             bool addedPiece = false;
@@ -100,10 +100,6 @@ namespace CIS153_Connect4_Group3
                 }
 
             }
-        }
-        public Square[,] getBoard()
-        {
-            return board;
         }
     }
 }
