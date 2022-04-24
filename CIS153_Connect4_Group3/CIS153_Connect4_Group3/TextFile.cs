2b10d90a37d9c695da5f65aae8fca9c6dad17b74
Datorel <toddo1225@gmail.com>
2022-04-24 19:57:00 -0400

    added framework for text file

---
+++ CIS153_Connect4_Group3/CIS153_Connect4_Group3/TextFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CIS153_Connect4_Group3
+{
+    class TextFile
+    {
+        private string fileName = "";
+        private char delim = ',';
+        
+
+        public List<Player> ReadList()
+        {
+            StreamReader file = new StreamReader(fileName);
+            String line = file.ReadLine();
+            List<Player> playerList = new List<Player>();
+
+
+            while (line != null)
+            {
+                //find first comma
+                int comma = line.IndexOf(delim);
+            }
+            return playerList;
+        }
+    }
+}
